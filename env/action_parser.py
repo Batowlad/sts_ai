57d127d0f6f08a5fe371c8ac320a68c23396fa43
Batowlad <wladyslaw.pshenychnyy@gmail.com>
2026-08-03 17:45:14 -0400

    Now pulling and caching all the functions names from GameInterface class for later use.

--- env/action_parser.py
+++ env/action_parser.py
@@ -1,14 +1,24 @@
 """Model text -> legal game action. TODO."""
 
 import ast
+from functools import cache
 
-game_interface = "env/game_interface.py"
+@cache
+def get_funcs():
+    with open("env/game_interface.py") as f:
+        tree = ast.parse(f.read())
 
-with open(game_interface, "r") as file:
-    tree = ast.parse(file.read())
+    funcs = []
 
-functions = [node.name for node in tree.body if isinstance(node, ast.FunctionDef)]
-print(functions)
+    for node in ast.walk(tree):
+        if isinstance(node, ast.ClassDef):
+            for item in node.body:
+                if isinstance(item, (ast.FunctionDef, ast.AsyncFunctionDef)):
+                    funcs.append(item.name)
 
-def parse_action(text: str, legal_actions):
-    raise NotImplementedError
+    return funcs
+
+def parse_action(text: str):
+    print(get_funcs())
+
+parse_action("")
