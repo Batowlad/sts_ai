b5c798519b3322a5d23f390f46cde6fbb0c5c53b
Batowlad <wladyslaw.pshenychnyy@gmail.com>
2026-08-03 00:08:50 -0400

    Idea for parsing action from llm. ts doesnt work...

--- env/action_parser.py
+++ env/action_parser.py
@@ -1,5 +1,14 @@
 """Model text -> legal game action. TODO."""
 
+import ast
+
+game_interface = "env/game_interface.py"
+
+with open(game_interface, "r") as file:
+    tree = ast.parse(file.read())
+
+functions = [node.name for node in tree.body if isinstance(node, ast.FunctionDef)]
+print(functions)
 
 def parse_action(text: str, legal_actions):
     raise NotImplementedError
